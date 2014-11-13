b21a2788dec6ad8f4bf0c2460215db10056c2d24
shyam <shyamsundar.shanmugam@wipro.com>
2014-11-13 11:05:16 +0530

    adding buzzer and controlling http://browser.zettajs.io

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-var zetta = require('zetta');
+var zetta = require('zetta'),
+   Buzzer = require('zetta-buzzer-edison-driver');
+
 
 // 172.16.1.16
 zetta()
   .name('xshyamx')
+  .use(Buzzer)
   .listen(1337);

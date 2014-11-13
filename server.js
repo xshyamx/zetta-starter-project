d24cf2454f3b8e61c59bdc56ea0f52aaf489ae10
shyam <shyamsundar.shanmugam@wipro.com>
2014-11-13 11:31:14 +0530

    linking microphone

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-var zetta = require('zetta'),
-   Buzzer = require('zetta-buzzer-edison-driver');
+var  zetta = require('zetta'),
+    Buzzer = require('zetta-buzzer-edison-driver'),
+Microphone = require('zetta-microphone-edison-driver');
 
 
 // 172.16.1.16
@@ -7,4 +8,5 @@ zetta()
   .name('xshyamx')
   .link('http://hello-zetta.herokuapp.com')
   .use(Buzzer)
+  .use(Microphone)
   .listen(1337);
